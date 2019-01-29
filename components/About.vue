52e5e139cf0d1e47fc2372304528c478e9fa6c0d
Aboo Thahir <thahulive@gmail.com>
2019-01-29 16:20:12 +0530

    removed unwanted files

---
+++ components/About.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="about-wrapper">
+    <v-container>
+      <div class="heading-block text-uppercase text-xs-center py-5">
+        <h3 class="subheading font-weight-regular">
+          My Name Is
+        </h3>
+        <h1 class="display-2 font-weight-bold">
+          Aboo Thahir
+        </h1>
+        <h2 class="title font-weight-light">
+          Full stack developer, Designer
+        </h2>
+      </div>
+      <div class="content-block">
+        <v-layout 
+          align-center 
+          justify-space-around 
+          row 
+          fill-height>
+          <v-flex 
+            md6
+            sm12
+            xs12
+            class="text-xs-justify px-2 mt-4">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </v-flex>
+          <v-flex 
+            md6
+            sm12
+            xs12>
+            features goes here
+          </v-flex>
+        </v-layout>
+      </div>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.about-wrapper {
+  min-height: 100vh;
+  color: #ffffff;
+  background-color: #ff3366;
+}
+.heading-block {
+}
+</style>
---
+++ components/Banner.vue
@@ -0,0 +1,46 @@
+<template>
+  <div
+    class="banner-wrapper">
+    <v-parallax
+      :height="windowSize.height"
+      src="/banner.jpg"
+    >
+      <v-layout
+        v-resize="onResize"
+        align-center
+        column
+        justify-center
+      >
+        <h1 class="display-2 font-weight-thin mb-3">Vuetify.js</h1>
+        <h4 class="subheading">Build your application today!</h4>
+      </v-layout>
+    </v-parallax>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      windowSize: {
+        width: 0,
+        height: 700
+      }
+    }
+  },
+  mounted() {
+    this.onResize()
+  },
+  methods: {
+    onResize() {
+      this.windowSize = { width: window.innerWidth, height: window.innerHeight }
+    }
+  }
+}
+</script>
+
+<style>
+.banner-wrapper {
+  background-color: #c9e1dd;
+}
+</style>
